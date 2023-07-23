86a0ebc83fdbfd5867480a7f4156806efe7b42b1
rxMATTEO <erik_sundberg@mail.ru>
2023-07-23 18:14:38 +0300

    Change eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import type {NuxtConfig} from "@nuxt/types";
+import type {NuxtConfig} from '@nuxt/types';
 
 const nuxtConfig: NuxtConfig = {
   devtools: {enabled: true},
   buildModules: ['@nuxt/typescript-build']
-}
+};
 
 export default nuxtConfig;
