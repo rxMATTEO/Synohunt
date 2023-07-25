a6909f7e2e38960fc18184c3fde5001c93cca282
rxMATTEO <erik_sundberg@mail.ru>
2023-07-25 13:02:20 +0300

    Add landing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,9 +19,23 @@
       <p>SynoHunt</p>
     </div>
     <div>
-      <div class="flex justify-content-between">
-        <div>a</div>
-        <div>a</div>
+      <div class="flex sm:justify-content-between sm:flex-row flex-column">
+        <p class="sm:text-6xl text-4xl font-bold">SynoHunt: The Ultimate Word Challenge!</p>
+        <p class="text-lg">Get ready to embark on a thrilling journey through the world of synonyms. Unleash your inner linguist, and dive into a realm of words and wonders with SynoHunt.</p>
+      </div>
+
+      <div class="col-12 md:col-12 lg:col-12 p-0 lg:pr-5 lg:pb-5 mt-4 lg:mt-0">
+        <div
+            style="height: 160px; padding: 2px; border-radius: 10px"
+        >
+          <div class="p-3 surface-card h-full" style="border-radius: 8px">
+            <div class="flex align-items-center justify-content-center bg-yellow-200 mb-3" style="width: 3.5rem; height: 3.5rem; border-radius: 10px">
+              <i class="pi pi-fw pi-users text-2xl"></i>
+            </div>
+            <h5 class="mb-2 text-900">Easy to Use</h5>
+            <span class="text-600">Posuere morbi leo urna molestie.</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
