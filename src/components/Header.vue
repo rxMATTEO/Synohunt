04c73c795c9b8d8be8abe8ae3cb056f5fc9ed4b7
rxMATTEO <erik_sundberg@mail.ru>
2023-07-28 15:57:35 +0300

    Add header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
+import {useSidebarStore} from '@/stores/sidebarStore';
+
+const {isExpanded} = useSidebarStore();
 const props = defineProps({
   expanded: Function,
 });
 </script>
 
 <template>
-  <div class="fixed surface-card shadow-8 w-full">
-    <Button @click="expanded">Hi</Button>
-    This is a header
+  <div :class="{expanded: isExpanded.value}" class="fixed surface-card shadow-8 right-0 header">
+    <div class="flex justify-content-between w-full">
+      <div>
+        <Button @click="expanded">Hi</Button>
+      </div>
+      <div>hiaasddsasadsadsdsa</div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="sass">
+.header
+  left: 0
+  &.expanded
+    left: 14rem // todo remove hardcode plz
 
 </style>
