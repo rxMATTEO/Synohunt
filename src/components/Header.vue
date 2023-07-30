a6b9f068e217aa92f5b34d53596c48aa551f70d8
rxMATTEO <erik_sundberg@mail.ru>
2023-07-30 17:35:57 +0300

    Add overlay panel for header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {onMounted, reactive} from 'vue';
+import {onMounted, reactive, ref} from 'vue';
 import {Theme, ThemesNames} from '@/app.config';
 import {useThemeStore} from '@/stores/themeStore';
 import {useSidebarStore} from '@/stores/sidebarStore';
@@ -10,10 +10,16 @@ defineProps({
 });
 const { getCurrentTheme, setCurrentTheme, currentTheme } = useThemeStore();
 let theme: {theme: Theme} = reactive({theme: {dark: true}});
+const op = ref();
+const toggle = (e) => op.value.toggle(e);
 onMounted(() => {
   getCurrentTheme();
   theme.theme = currentTheme;
 });
+
+function onMailMouseOver(e){
+  toggle(e);
+}
 
 function changeTheme(){
   const light = ThemesNames.light;
@@ -32,8 +38,24 @@ function changeTheme(){
           <i class="pi pi-align-justify text-2xl vertical-align-middle"></i>
         </Button>
       </div>
-      <div class="t-h-[20px]">
-        <Button unstyled @click="changeTheme">
+      <div class="t-h-[20px] flex t-items-center">
+        <div class="mr-3" @mouseover="onMailMouseOver" @mouseleave="onMailMouseOver">
+          <i v-badge.danger class="pi pi-envelope p-text-secondary"></i>
+        </div>
+        <OverlayPanel ref="op">
+          <p>Тут типа сообщения</p>
+        </OverlayPanel>
+        <div>
+          <Avatar v-badge.success="4" icon="pi pi-user" class="mr-3" size="small"></Avatar>
+        </div>
+        <div class="badges">
+          <Badge :value="2" class="mr-2"></Badge>
+          <Badge :value="8" severity="success" class="mr-2"></Badge>
+          <Badge :value="4" severity="info" class="mr-2"></Badge>
+          <Badge :value="12" severity="warning" class="mr-2"></Badge>
+          <Badge :value="3" severity="danger"></Badge>
+        </div>
+        <Button unstyled @click="changeTheme" class="ml-5 t-h-[20px]">
           <i v-if="theme.theme.dark" class="pi pi-moon text-xl cursor-pointer t-fill-black"></i>
           <i v-else-if="theme.theme.light" class="pi pi-moon text-xl cursor-pointer t-fill-black"></i>
         </Button>
--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -2,9 +2,18 @@ import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import { defineNuxtPlugin } from 'nuxt/app';
 import Dropdown from 'primevue/dropdown';
+import Badge from 'primevue/badge';
+import Avatar from 'primevue/avatar';
+import BadgeDirective from 'primevue/badgedirective';
+import OverlayPanel from 'primevue/overlaypanel';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+  nuxtApp.vueApp.directive('badge', BadgeDirective);
+  
   nuxtApp.vueApp.component('Button', Button);
   nuxtApp.vueApp.component('Dropdown', Dropdown);
+  nuxtApp.vueApp.component('Badge', Badge);
+  nuxtApp.vueApp.component('Avatar', Avatar);
+  nuxtApp.vueApp.component('OverlayPanel', OverlayPanel);
 });
