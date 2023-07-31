ad8e78a19b30b754a9ce669a909238acc0a93a87
rxMATTEO <erik_sundberg@mail.ru>
2023-07-31 13:39:36 +0300

    Edit links in sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,25 +14,73 @@ const props = defineProps({
           <img src="/img/download.webp" class="block mx-auto w-full" alt="logo" draggable="false"  />
         </NuxtLink>
       </div>
-      <div class="mt-5 text-secondary" v-if="isExpanded.value">
-        <p class="px-1">Training</p>
+      <div class="mt-5 text-secondary t-text-sm" v-if="isExpanded.value">
+        <p class="px-2 uppercase text-color-secondary">Training</p>
         <div class="mt-1">
-          <NuxtLink to="/" class="hover:surface-200 block">
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
             <div class="flex px-1">
               <div class="t-w-1/6">a</div>
               <div class="t-w-5/6">
                 <p class="t-font-bold">Practice</p>
-                <p class="text-sm">Complete tasks and shit</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
+              </div>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
+              <div class="t-w-1/6">a</div>
+              <div class="t-w-5/6">
+                <p class="t-font-bold">Practice</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
               </div>
             </div>
           </NuxtLink>
 
-          <NuxtLink to="/">
-            <div class="flex">
+          <p class="px-2 uppercase text-color-secondary mt-3">CAREER</p>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
               <div class="t-w-1/6">a</div>
               <div class="t-w-5/6">
                 <p class="t-font-bold">Practice</p>
-                <p class="text-sm">Complete tasks and shit</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
+              </div>
+            </div>
+          </NuxtLink>
+          <p class="px-2 uppercase text-color-secondary mt-3">Community</p>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
+              <div class="t-w-1/6">a</div>
+              <div class="t-w-5/6">
+                <p class="t-font-bold">Practice</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
+              </div>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
+              <div class="t-w-1/6">a</div>
+              <div class="t-w-5/6">
+                <p class="t-font-bold">Practice</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
+              </div>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
+              <div class="t-w-1/6">a</div>
+              <div class="t-w-5/6">
+                <p class="t-font-bold">Practice</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
+              </div>
+            </div>
+          </NuxtLink>
+          <p class="px-2 uppercase text-color-secondary mt-3">About</p>
+          <NuxtLink to="/" class="hover:surface-200 block transition-colors t-ease-in-out t-duration-200">
+            <div class="flex px-1">
+              <div class="t-w-1/6">a</div>
+              <div class="t-w-5/6">
+                <p class="t-font-bold">Practice</p>
+                <p class="text-sm text-color-secondary">Complete tasks and shit</p>
               </div>
             </div>
           </NuxtLink>
