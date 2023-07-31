34de4e1b7a5d2151455c8c7eb31c400790c0f6b8
rxMATTEO <erik_sundberg@mail.ru>
2023-07-31 11:39:55 +0300

    Footer base layout done

--- src/layouts/footer.vue
+++ src/layouts/footer.vue
@@ -5,8 +5,14 @@
 <template>
 <div>
   <slot />
-  <div>
-    This is footer
+  <div class="lg:px-8 lg:mx-8 px-3 mt-5">
+    <div class="flex t-items-center t-justify-center">
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+        <span>2</span>
+    </div>
   </div>
 </div>
 </template>
