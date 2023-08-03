2b6538a9404fcb6ffae2f5d0ff994404e840eb4f
rxMATTEO <erik_sundberg@mail.ru>
2023-08-03 16:50:35 +0300

    Origin moved to .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ const nuxtConfig = {
   ],
   auth: {
     isEnabled: true,
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN,
     basePath: '/api/auth',
     globalAppMiddleware: true,
   },
