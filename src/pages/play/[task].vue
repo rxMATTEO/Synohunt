8e62f20bc0218129df3d6042921e39523f509c54
rxMATTEO <erik_sundberg@mail.ru>
2023-08-03 16:24:10 +0300

    Add play page

---
+++ src/pages/play/[task].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="header-n-sidebar">
+      {{route.params.task}}
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
