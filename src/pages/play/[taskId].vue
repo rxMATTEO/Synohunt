cccac960569d4f595617bd266f9c33bfd5c10d2f
rxMATTEO <erik_sundberg@mail.ru>
2023-08-05 13:54:05 +0300

    Add syno hunt :)

--- src/pages/play/[taskId].vue
+++ src/pages/play/[taskId].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 type RouteParams = {
   params: {
@@ -26,40 +26,73 @@ type TaskResponse = {
     Synonym: Syno[],
   },
   task: Task[]
-
 }
-const task = reactive<{value: TaskResponse}>({ value: (await useFetch(`/api/task/${route.params.taskId}`)).data });
 
+function checkUserInput (e: KeyboardEvent) {
+  if (e.key === 'Enter') {
+    const foundSynoIndex = synonyms.findIndex(syno => syno.value === userSyno.value);
+    if (foundSynoIndex !== -1) {
+      solvedSynonyms.value.push(synonyms[foundSynoIndex]);
+      synonyms.splice(foundSynoIndex, 1);
+    }
+  }
+}
+
+const task = reactive<{value: TaskResponse}>({ value: (await useFetch(`/api/task/${route.params.taskId}`)).data });
+const synonyms = task.value.Word.Synonym;
+const solvedSynonyms = reactive({ value: [] });
+const userSyno = ref('');
 </script>
 
 <template>
   <div>
     <NuxtLayout name="header-n-sidebar">
-      <div class="lg:px-8 lg:mx-8 px-3">
-        <h1>Guess the all synonyms</h1>
-        <span v-for="word in task.value.description.split(' ')">
-          <span :class="{'bg-blue-500': word === task.value.Word.word} ">{{ word + " " }}</span>
-        </span>
-
-        <div class="flex t-place-content-between">
-          <div class="t-w-1/5">
-            <p>All synonyms</p>
-            <p v-for="(syno) in task.value.Word.Synonym">
-              {{ syno.value }}
+      <div class="lg:px-8 lg:mx-8 px-3" @keyup="checkUserInput">
+        <div class="surface-ground t-rounded-md p-5">
+          <h1 class="text-4xl">
+            Guess the all synonyms
+          </h1>
+          <div class="my-5">
+            <p class="text-2xl">
+              Context:
             </p>
+            <span v-for="word in task.value.description.split(' ')">
+              <span :class="{'bg-primary-500': word === task.value.Word.word} ">{{ word + " " }}</span>
+            </span>
           </div>
-          <div class="bg-cyan-50 t-w-3/5">
-            a
-          </div>
-          <div class="t-w-1/5 text-right">
-            <p>Solved synonyms</p>
+
+          <div class="flex t-place-content-between">
+            <div class="t-w-1/5">
+              <p class="text-xl">
+                All synonyms:
+              </p>
+              <div class="mt-5">
+                <p v-for="(syno) in synonyms">
+                  {{ syno.value }}
+                </p>
+              </div>
+            </div>
+            <div class="t-w-3/5">
+              <p class="text-8xl text-center t-h-1/2">
+                {{ userSyno || "" }}
+              </p>
+              <p class="p-float-label w-full">
+                <InputText id="user-syno" v-model="userSyno" class="w-full" />
+                <label for="user-syno">Synonym</label>
+              </p>
+              <div class="mx-auto mt-5">
+                <Button label="Guess" type="null" class="text-center block mx-auto px-5 py-3 animated-gradient-rainbow" @click="checkUserInput" />
+              </div>
+            </div>
+            <div class="t-w-1/5 text-right">
+              <p>Solved synonyms</p>
+              <p v-for="(syno) in solvedSynonyms.value">
+                {{ syno.value }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </NuxtLayout>
   </div>
 </template>
-
-<style scoped lang="sass">
-
-</style>
