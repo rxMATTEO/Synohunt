e3c5ad5bf4784aa56acb852c3f5d16f6e856a1b5
rxMATTEO <erik_sundberg@mail.ru>
2023-08-13 18:14:23 +0300

    Add notification on level upgrade

--- src/stores/levelStore.ts
+++ src/stores/levelStore.ts
@@ -1,14 +1,17 @@
 import { reactive } from 'vue';
 import { defineStore, useAuth, useFetch } from '#imports';
+import {useNotificationsStore} from "@/stores/notificationsStore";
 
 export const useLevelStore = defineStore('levelStore', () => {
   const { data: { value: { user: { account } } } } = useAuth();
   const level = reactive({ value: account.Level.value });
+  const notificationsStore = useNotificationsStore();
 
   function getLvl () {
     return level.value;
   }
   function upgradeLvl () {
+    notificationsStore.addNotification({description: 'Congratz', title: 'Hi', secondaryText: 'u like level upgrade and shit'});
     level.value = +level.value + 1;
   }
   return ({
