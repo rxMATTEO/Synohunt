2ca64186982c7b76ab78de6ecb92508f968ab842
rxMATTEO <erik_sundberg@mail.ru>
2023-08-15 16:12:06 +0300

    Gradient box improvings

--- src/components/CreatedTasks.vue
+++ src/components/CreatedTasks.vue
@@ -2,12 +2,6 @@
 import { Word, Synonym, Difficulity, Tag, Language, Task } from '@prisma/client';
 
 const { data: { value: { user: { account } } } } = useAuth();
-const createdTasks = ref((await useFetch('/api/task/created', {
-  method: 'POST',
-  body: {
-    userId: account.id
-  }
-})).data.value);
 
 type CompletedTasksResponse = Task & {
   Word: Word & { Synonym: Synonym[] }
@@ -16,8 +10,16 @@ type CompletedTasksResponse = Task & {
   Tag: Tag,
   _count: { CompletedTask: number }
 }
-</script>
 
+const createdTasks = ref((await useFetch('/api/task/created', {
+  method: 'POST',
+  body: {
+    userId: account.id
+  }
+})).data.value as CompletedTasksResponse[]);
+
+// mb add coins that was spend on task and creator can loot them
+</script>
 <template>
   <DataView
     :value="createdTasks"
@@ -27,7 +29,7 @@ type CompletedTasksResponse = Task & {
     <template #list="slotProps: { data: CompletedTasksResponse }">
       <GradientBox :gradient-name="slotProps.data.Difficulity.name.toLowerCase()" class="mb-3">
         <template #left-side>
-          <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+          <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4 p-3">
             <div class="flex flex-column align-items-center sm:align-items-start gap-3">
               <div class="text-2xl font-bold text-900">
                 <p v-html="slotProps.data.description" />
@@ -40,15 +42,23 @@ type CompletedTasksResponse = Task & {
                 <Tag :value="slotProps.data.Tag" severity="" />
               </div>
             </div>
+          </div>
+        </template>
+        <template #right-side>
+          <div class="p-3 relative">
+            <TinyCoin class="absolute right-0" :value="slotProps.data._count.CompletedTask.toString()" />
+            <TinyCoin
+              class="absolute right-0 bottom-0"
+              :value="slotProps.data.Word.Synonym.reduce((prev: 0, current: Synonym) => {
+                return prev + current.pointsForGuess;
+              }, 0).toString()"
+            />
             <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
               <div v-for="syno in slotProps.data.Word.Synonym">
                 <p>{{ syno.value }}</p>
               </div>
             </div>
           </div>
-        </template>
-        <template #right-side>
-          <TinyCoin :value="slotProps.data._count.CompletedTask.toString()" />
         </template>
       </GradientBox>
     </template>
