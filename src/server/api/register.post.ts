1cb30a267c2380d0c7489ce976acd530ad859878
rxMATTEO <erik_sundberg@mail.ru>
2023-08-24 17:21:14 +0300

    Remove uuid

--- src/server/api/register.post.ts
+++ src/server/api/register.post.ts
@@ -1,14 +1,11 @@
 import { readBody } from 'h3';
-import { v4 as uuidv4 } from 'uuid';
 export default eventHandler(async (event) => {
   BigInt.prototype.toJSON = function () { return this.toString(); };
   const { username, email } = await readBody(event);
-  const id = uuidv4();
   const account = await event.context.prisma.user.create({
     data: {
       email,
-      name: username,
-      id,
+      name: username, // todo add default image
       points: 0,
       registrationDate: Date.now()
     }
