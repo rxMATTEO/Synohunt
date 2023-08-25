f3fd2152b38e87db211244f54d48c37770e709ea
rxMATTEO <erik_sundberg@mail.ru>
2023-08-25 10:58:42 +0300

    Add validation for all data on register page

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -12,55 +12,59 @@ definePageMeta({
   }
 });
 
-const mediumRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})	/;
+const mediumRegex =
+  /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
 const strongRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
 
 const { handleSubmit, resetForm } = useForm();
-const { value: password, errorMessage } = useField('value', validateField);
+const { value: password, errorMessage } = useField(
+  'password',
+  validateWeakness
+);
+const { value: email, errorMessage: errorMailMessage } = useField(
+  'email',
+  value => validateEmpty(value, 'Email')
+);
+const { value: username, errorMessage: errorUsernameMessage } = useField(
+  'username',
+  value => validateEmpty(value, 'Username')
+);
 
-function validateField (value) {
-  console.log(value);
-  if (value.match(mediumRegex) || value.match(strongRegex)) {
-    return true;
-  }
-  return 'Password is required.';
+type FieldName = 'Password' | 'Email' | 'Username';
+
+function validateEmpty (value: string, fieldName: FieldName) {
+  return !value ? `${fieldName} is required` : true;
 }
 
-const onSubmit = handleSubmit((values) => {
-  if (values.value && values.value.length > 0) {
-    toast.add({ severity: 'info', summary: 'Form Submitted', detail: values.value, life: 3000 });
-    resetForm();
+function validateWeakness (password: string) {
+  if (!password) {
+    return 'Password is required.';
   }
-});
+  if (password.match(mediumRegex) || password.match(strongRegex)) {
+    return true;
+  } else {
+    return 'Password is weak';
+  }
+}
 
-const registerUser = handleSubmit(async (values) => {
-  if (!values.value && !values.value.length > 0) { return false; }
-  const result = await $fetch('/api/jwk', {
+const registerUser = handleSubmit(async ({ email, password, username }) => {
+  console.log('registering !');
+  const result = (await $fetch('/api/jwk', {
     method: 'POST'
-  }) as Awaited<JwkResponse>;
-  const creds = Buffer.Buffer.from(JSON.stringify({ email: email.value, password: password.value, username: username.value }));
+  })) as Awaited<JwkResponse>;
+  const creds = Buffer.Buffer.from(
+    JSON.stringify({ email, password, username })
+  );
   const encrypted = JSON.stringify(await rsa.encrypt(creds, result.publicKey));
   const responseCreds = await $fetch('/api/credentials', {
     method: 'POST',
     body: {
       data: encrypted,
       id: result.id,
-      email: email.value
+      email
     }
   });
-  // return await $fetch('/api/register', {
-  //   method: 'POST',
-  //   body: {
-  //     username: username.value,
-  //     password: password.value,
-  //     email: email.value
-  //   }
-  // });
 });
-
-const username = ref('');
-const email = ref('');
-
 </script>
 
 <template>
@@ -68,35 +72,59 @@ const email = ref('');
     <div class="surface-card p-4 shadow-2 border-round w-full lg:w-4">
       <div class="text-center mb-5">
         <div class="t-h-32 relative top-0 flex justify-content-center">
-          <img src="/img/biglogo.png" alt="Image" class="text-center mb-3 h-full overflow-hidden absolute top-0">
+          <img
+            src="/img/biglogo.png"
+            alt="Image"
+            class="text-center mb-3 h-full overflow-hidden absolute top-0"
+          >
         </div>
         <div class="text-900 text-3xl font-medium mb-3">
           Register
         </div>
         <span class="text-600 font-medium line-height-3">Already have an account?</span>
-        <NuxtLink to="login" class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">
+        <NuxtLink
+          to="login"
+          class="font-medium no-underline ml-2 text-blue-500 cursor-pointer"
+        >
           Login now!
         </NuxtLink>
       </div>
 
       <div>
         <form @submit="registerUser">
-          <label for="username" class="block text-900 font-medium mb-2">Username</label>
-          <InputText id="username" v-model="username" type="text" class="w-full mb-3" />
+          <label
+            for="username"
+            class="block text-900 font-medium mb-2"
+          >Username</label>
+          <InputText
+            id="username"
+            v-model="username"
+            :class="{ 'p-invalid': errorMessage }"
+            type="text"
+            class="w-full"
+          />
+          <small id="text-error" class="p-error">{{
+            errorUsernameMessage || "&nbsp;"
+          }}</small>
 
-          <label for="value" class="block text-900 font-medium mb-2">Password</label>
+          <label
+            for="password"
+            class="block text-900 font-medium mb-2"
+          >Password</label>
           <Password
             id="value"
             v-model="password"
+            :medium-regex="'^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'"
+            :strong-regex="'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})'"
             type="text"
             :class="{ 'p-invalid': errorMessage }"
             aria-describedby="text-error"
             toggle-mask
-            class="w-full mb-3"
+            class="w-full"
             :pt="{
               root: {
                 class: 'w-full',
-              }
+              },
             }"
             input-class="w-full"
           >
@@ -117,13 +145,33 @@ const email = ref('');
               </ul>
             </template>
           </Password>
-          <small id="text-error" class="p-error">{{ errorMessage || '&nbsp;' }}</small>
+          <small id="text-error" class="p-error">{{
+            errorMessage || "&nbsp;"
+          }}</small>
 
-          <label for="email" class="block text-900 font-medium mb-2">Email</label>
-          <InputText id="email" v-model="email" type="text" class="w-full mb-3" />
+          <label
+            for="email"
+            class="block text-900 font-medium mb-2"
+          >Email</label>
+          <InputText
+            id="email"
+            v-model="email"
+            aria-describedby="text-error"
+            :class="{ 'p-invalid': errorMailMessage }"
+            type="text"
+            class="w-full"
+          />
+          <small id="text-error" class="p-error">{{
+            errorMailMessage || "&nbsp;"
+          }}</small>
 
           <div class="mt-6 prevent-tw">
-            <Button label="Register" icon="pi pi-user-plus" type="submit" class="w-full" />
+            <Button
+              label="Register"
+              icon="pi pi-user-plus"
+              type="submit"
+              class="w-full"
+            />
           </div>
         </form>
       </div>
@@ -131,6 +179,4 @@ const email = ref('');
   </div>
 </template>
 
-<style scoped lang="sass">
-
-</style>
+<style scoped lang="sass"></style>
