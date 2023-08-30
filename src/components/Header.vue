c884bb3cecb4bf32de357043a9008d559e379953
rxmatteo <erik_sundberg@mail.ru>
2023-08-30 09:37:02 +0300

    Fix bug when header is stuck even on pc tho

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@ import StickyElement from 'vue-sticky-element';
 import { useMoneyStore } from '../stores/moneyStore';
 import { useMessageStore } from '../stores/messageStore';
 import { useAuth } from '#imports';
-import { Theme, ThemesNames } from '@/app.config';
+import { Platforms, Theme, ThemesNames } from '@/app.config';
 import { useThemeStore } from '@/stores/themeStore';
 import { useSidebarStore } from '@/stores/sidebarStore';
 import ChangeThemeButton from '@/components/ChangeThemeButton.vue';
@@ -65,10 +65,16 @@ const { currentMoney } = storeToRefs(moneyStore);
 
 const messagesStore = useMessageStore();
 const { messages } = storeToRefs(messagesStore);
+
+const forceStuck = computed(() => {
+  const appConfig = useAppConfig();
+  const platform = appConfig.platform;
+  return platform === Platforms.mobile && isExpanded.value;
+});
 </script>
 
 <template>
-  <StickyElement stuck-class="vue-sticky-element--stuck width-reset" :force-show="isExpanded.value">
+  <StickyElement stuck-class="vue-sticky-element--stuck width-reset" :force-show="forceStuck">
     <div :class="{'!t-left-56': isExpanded.value, 'left-0': !isExpanded.value}" class=" t-z-10 px-2 max-md:!t-left-0 t-transition-all t-ease-in-out t-duration-200 fixed surface-card shadow-8 right-0 header">
       <div class="flex align-items-center justify-content-between w-full t-py-[.7rem]">
         <div>
