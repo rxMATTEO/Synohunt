57447f7f35a9fe56fe5d97a1eb019ce7107a899a
rxmatteo <erik_sundberg@mail.ru>
2023-09-01 11:34:10 +0300

    Footer fix

--- src/layouts/footer.vue
+++ src/layouts/footer.vue
@@ -5,20 +5,17 @@
 <template>
   <div>
     <slot />
-    <PaddingBox class="mt-5">
-      <div class="flex t-items-center t-justify-center">
-        <a href="https://github.com/volodin-artem/Synohunt/" class=" hover:text-primary transition-colors t-duration-200 t-ease-in-out" target="_blank">
-          <span>Contribute on Github</span>
-          <i class="pi pi-fw pi-github" />
-        </a>
-        <NuxtLink to="/about" class="mx-3 hover:text-primary transition-colors t-duration-200 t-ease-in-out">
-          About
-        </NuxtLink>
-        <a href="/" class=" hover:text-primary transition-colors t-duration-200 t-ease-in-out">Blog</a>
-        <a href="/" class="mx-3  hover:text-primary transition-colors t-duration-200 t-ease-in-out">Privacy</a>
-        <a href="/" class=" hover:text-primary transition-colors t-duration-200 t-ease-in-out">Contact</a>
-      </div>
-    </PaddingBox>
+    <div class="flex t-items-center t-justify-center mt-5 flex-wrap">
+      <a href="https://github.com/volodin-artem/Synohunt/" class=" hover:text-primary transition-colors t-duration-200 t-ease-in-out" target="_blank">
+        <span>Contribute on Github</span>
+        <i class="pi pi-fw pi-github" />
+      </a>
+      <NuxtLink to="/about" class="mx-3 hover:text-primary transition-colors t-duration-200 t-ease-in-out">
+        About
+      </NuxtLink>
+      <a href="/" class="mx-3  hover:text-primary transition-colors t-duration-200 t-ease-in-out">Privacy</a>
+      <a href="/" class=" hover:text-primary transition-colors t-duration-200 t-ease-in-out">Contact</a>
+    </div>
   </div>
 </template>
 
